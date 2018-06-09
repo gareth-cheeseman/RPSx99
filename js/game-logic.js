c75962d23b859a4566c523a1f20586dd7da02815
gareth-cheeseman <garethcheeseman@humbug.local>
2018-06-09 12:56:16 +0100

    Adds global variables and setPlayerMoves

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,45 @@
 // All code should be written in this file.
+let playerOneMoveOneType;
+let playerOneMoveOneValue;
+
+let playerOneMoveTwoType;
+let playerOneMoveTwoValue;
+
+let playerOneMoveThreeType;
+let playerOneMoveThreeValue;
+
+let playerTwoMoveOneType;
+let playerTwpMoveOneValue;
+
+let playerTwoMoveTwoType;
+let playerTwoMoveTwoValue;
+
+let playerTwoMoveThreeType;
+let playerTwoMoveThreeValue;
+
+const setPlayerMoves = (
+  player,
+  moveOneType,
+  moveOneValue,
+  moveTwoType,
+  moveTwoValue,
+  moveThreeType,
+  moveThreeValue
+) => {
+    if (player === 'Player One'){
+        playerOneMoveOneType = moveOneType;
+        playerOneMoveOneValue = moveOneValue;
+        playerOneMoveTwoType = moveTwoType;
+        playerOneMoveTwoValue = moveTwoValue;
+        playerOneMoveThreeType = moveThreeType;
+        playerOneMoveThreeValue = moveThreeValue;
+    } else {
+        playerTwoMoveOneType = moveOneType;
+        playerTwoMoveOneValue = moveOneValue;
+        playerTwoMoveTwoType = moveTwoType;
+        playerTwoMoveTwoValue = moveTwoValue;
+        playerTwoMoveThreeType = moveThreeType;
+        playerTwoMoveThreeValue = moveThreeValue;
+    }
+};
+
