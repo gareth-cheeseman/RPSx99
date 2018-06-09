922b33b6b50ffa69fcb9f2d063bae060238ef731
gareth-cheeseman <garethcheeseman@humbug.local>
2018-06-09 16:49:14 +0100

    Adds guards for setPlayerMoves edge cases

--- js/game-logic.js
+++ js/game-logic.js
@@ -26,93 +26,128 @@ const setPlayerMoves = (
   moveThreeType,
   moveThreeValue
 ) => {
-    if (player === 'Player One'){
-        playerOneMoveOneType = moveOneType;
-        playerOneMoveOneValue = moveOneValue;
-        playerOneMoveTwoType = moveTwoType;
-        playerOneMoveTwoValue = moveTwoValue;
-        playerOneMoveThreeType = moveThreeType;
-        playerOneMoveThreeValue = moveThreeValue;
-    } else {
-        playerTwoMoveOneType = moveOneType;
-        playerTwoMoveOneValue = moveOneValue;
-        playerTwoMoveTwoType = moveTwoType;
-        playerTwoMoveTwoValue = moveTwoValue;
-        playerTwoMoveThreeType = moveThreeType;
-        playerTwoMoveThreeValue = moveThreeValue;
-    }
+  const parameters = [
+    player,
+    moveOneType,
+    moveOneValue,
+    moveTwoType,
+    moveTwoValue,
+    moveThreeType,
+    moveThreeValue
+  ];
+
+  if (parameters.includes(undefined)) {
+    return;
+  }
+
+  if (parameters.some(x => x < 1 || x > 99)) {
+    return;
+  }
+
+  if (moveOneValue + moveTwoValue + moveThreeValue > 99) {
+    return;
+  }
+
+  if (
+    ![moveOneType, moveTwoType, moveThreeType].every(
+      x => x === 'rock' || x === 'paper' || x === 'scissors'
+    )
+  ) {
+    return;
+  }
+
+  if (player === 'Player One') {
+    playerOneMoveOneType = moveOneType;
+    playerOneMoveOneValue = moveOneValue;
+    playerOneMoveTwoType = moveTwoType;
+    playerOneMoveTwoValue = moveTwoValue;
+    playerOneMoveThreeType = moveThreeType;
+    playerOneMoveThreeValue = moveThreeValue;
+  } else if (player === 'Player Two') {
+    playerTwoMoveOneType = moveOneType;
+    playerTwoMoveOneValue = moveOneValue;
+    playerTwoMoveTwoType = moveTwoType;
+    playerTwoMoveTwoValue = moveTwoValue;
+    playerTwoMoveThreeType = moveThreeType;
+    playerTwoMoveThreeValue = moveThreeValue;
+  } else {
+    return;
+  }
 };
 
-const getRoundWinner = (roundNumber) => {
-    const playerOne = 'Player One';
-    const playerTwo = 'Player Two';
-    let playerOneMoveType;
-    let playerOneMoveValue;
-    let playerTwoMoveType;
-    let playerTwoMoveValue;
+const getRoundWinner = roundNumber => {
+  const playerOne = 'Player One';
+  const playerTwo = 'Player Two';
+  let playerOneMoveType;
+  let playerOneMoveValue;
+  let playerTwoMoveType;
+  let playerTwoMoveValue;
 
-    switch (roundNumber) {
-        case 1:
-            playerOneMoveType = playerOneMoveOneType;
-            playerOneMoveValue = playerOneMoveOneValue;
-            playerTwoMoveType = playerTwoMoveOneType;
-            playerTwoMoveValue = playerTwoMoveOneValue;
-            break;
-        case 2:
-            playerOneMoveType = playerOneMoveTwoType;
-            playerOneMoveValue = playerOneMoveTwoValue;
-            playerTwoMoveType = playerTwoMoveTwoType;
-            playerTwoMoveValue = playerTwoMoveTwoValue;
-            break;
-    
-        case 3:
-            playerOneMoveType = playerOneMoveThreeType;
-            playerOneMoveValue = playerOneMoveThreeValue;
-            playerTwoMoveType = playerTwoMoveThreeType;
-            playerTwoMoveValue = playerTwoMoveThreeValue;
-            break;
-        default:
-         return null;
-        break;
+  switch (roundNumber) {
+    case 1:
+      playerOneMoveType = playerOneMoveOneType;
+      playerOneMoveValue = playerOneMoveOneValue;
+      playerTwoMoveType = playerTwoMoveOneType;
+      playerTwoMoveValue = playerTwoMoveOneValue;
+      break;
+    case 2:
+      playerOneMoveType = playerOneMoveTwoType;
+      playerOneMoveValue = playerOneMoveTwoValue;
+      playerTwoMoveType = playerTwoMoveTwoType;
+      playerTwoMoveValue = playerTwoMoveTwoValue;
+      break;
+
+    case 3:
+      playerOneMoveType = playerOneMoveThreeType;
+      playerOneMoveValue = playerOneMoveThreeValue;
+      playerTwoMoveType = playerTwoMoveThreeType;
+      playerTwoMoveValue = playerTwoMoveThreeValue;
+      break;
+    default:
+      return null;
+      break;
+  }
+
+  if (
+    !playerOneMoveType ||
+    !playerOneMoveValue ||
+    !playerTwoMoveType ||
+    !playerTwoMoveValue
+  ) {
+    return null;
+  }
+
+  if (playerOneMoveType === playerTwoMoveType) {
+    if (playerOneMoveValue > playerTwoMoveValue) {
+      return playerOne;
+    } else if (playerOneMoveValue === playerTwoMoveValue) {
+      return 'Tie';
+    } else {
+      return playerTwo;
     }
+  }
 
-    if(!playerOneMoveType ||!playerOneMoveValue || !playerTwoMoveType || !playerTwoMoveValue ){
-        return null;
+  if (playerOneMoveType === 'rock') {
+    if (playerTwoMoveType === 'paper') {
+      return playerTwo;
+    } else {
+      return playerOne;
     }
+  }
 
-    if (playerOneMoveType === playerTwoMoveType){
-        if (playerOneMoveValue > playerTwoMoveValue){
-            return playerOne;
-        } else if (playerOneMoveValue === playerTwoMoveValue){
-            return 'Tie';
-        } else {
-            return playerTwo;
-        }
+  if (playerOneMoveType === 'paper') {
+    if (playerTwoMoveType === 'scissors') {
+      return playerTwo;
+    } else {
+      return playerOne;
     }
+  }
 
-    if (playerOneMoveType === 'rock') {
-        if (playerTwoMoveType === 'paper'){
-            return playerTwo;
-        } else {
-            return playerOne;
-        }
+  if (playerOneMoveType === 'scissors') {
+    if (playerTwoMoveType === 'rock') {
+      return playerTwo;
+    } else {
+      return playerOne;
     }
-
-    if (playerOneMoveType === 'paper') {
-        if (playerTwoMoveType === 'scissors'){
-            return playerTwo;
-        } else {
-            return playerOne;
-        }
-    }
-
-    if (playerOneMoveType === 'scissors') {
-        if (playerTwoMoveType === 'rock'){
-            return playerTwo;
-        } else {
-            return playerOne;
-        }
-    }
-
-}
-
+  }
+};
